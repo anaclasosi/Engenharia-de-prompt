23099ec5c4f91e0ea4947774d381f2c855310764
Ana Clara <anaclasosi@gmail.com>
2025-01-17 00:33:10 -0300

    Página de pedidos

--- src/pages/FazerPedidoPage.js
+++ src/pages/FazerPedidoPage.js
@@ -18,9 +18,9 @@ const FazerPedidoPage = ({ order, handleConfirmPayment }) => {
     <div className="fazer-pedido-page">
       <Header /> {/* Barra de navegação no topo */}
       <div className="overlay">
-        <main className="flex flex-col items-center mt-6">
+        <main className="flex flex-col items-center justify-center mt-6">
           {Object.keys(categories).map((category) => (
-            <div key={category}>
+            <div key={category} className="category-container">
               <button
                 className="category-title"
                 onClick={() => setSelectedCategory(category)}
