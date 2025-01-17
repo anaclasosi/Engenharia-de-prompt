3fccf8129fc20eb75318cb1c0a9ddb80c652be27
Ana Clara <anaclasosi@gmail.com>
2025-01-17 17:40:10 -0300

    Pagina de acompanhamento atualizada

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import './Header.css'; // Importando o CSS
+import './Header.css';
+import logo from '../Assets/background.png'; // Importar a imagem background.png como logo
 
 const Header = () => {
   return (
     <header className="header">
+      <img src={logo} alt="Logo" className="logo" /> {/* Adicionar a logo */}
       <nav className="nav">
         <Link to="/" className="nav-link">Home</Link>
         <Link to="/fazer-pedido" className="nav-link">Fazer Pedido</Link>
